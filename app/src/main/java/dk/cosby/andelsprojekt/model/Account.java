d457d3391eeafb299657c8babca715fb8b729ea2
Nicklas Nielsen <nicklas.bring.nielsen@gmail.com>
2019-05-25 15:57:09 +0200

    oprettet en account klasse som indeholder éns balance

---
+++ app/src/main/java/dk/cosby/andelsprojekt/model/Account.java
@@ -0,0 +1,35 @@
+package dk.cosby.andelsprojekt.model;
+
+public class Account {
+
+    private int accountID;
+    private String userID;
+    private double balance;
+
+    public Account() {
+    }
+
+    public int getAccountID() {
+        return accountID;
+    }
+
+    public void setAccountID(int accountID) {
+        this.accountID = accountID;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+}
